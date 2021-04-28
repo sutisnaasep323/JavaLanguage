27885c8c1c6007f7937642046f191824ea35a3b2
yummicodes <yummicodes@gmail.com>
2021-04-28 13:06:30 +0700

    Pembuatan objek

---
+++ src/OOP/PZN/PersonApp.java
@@ -0,0 +1,22 @@
+package OOP.PZN;
+
+public class PersonApp {
+
+    public static void main(String[] args) {
+        //Pembuatan Objek
+        //1
+        Person person1 = new Person();
+
+        //2
+        Person person2;
+        person2 = new Person();
+
+        //3
+        var person3 = new Person();
+
+        System.out.println(person1);
+        System.out.println(person2);
+        System.out.println(person3);
+    }
+
+}
