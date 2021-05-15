a5db9fce4d3d5751b00f6f349298f4ca70f9a018
yummicodes <yummicodes@gmail.com>
2021-05-15 22:21:03 +0700

    Latihan_Exercise 19 (20) = desimal to Hexa

---
+++ src/Latihan/w3resource/BasicPart1/Exercise19.java
@@ -0,0 +1,15 @@
+package Latihan.w3resource.BasicPart1;
+
+public class Exercise19 {
+    // desimal to hexa desimal
+
+    public static void main(String[] args) {
+
+        int a = 15;
+
+        String hexa = Integer.toHexString(a);
+
+        System.out.println("Hexa desimal dari = " + a + " adalah : " + hexa);
+
+    }
+}
