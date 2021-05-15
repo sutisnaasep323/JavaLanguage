82c3795ad81669a43f6704a5dc9f0b7d944bec83
yummicodes <yummicodes@gmail.com>
2021-05-15 22:24:02 +0700

    Latihan_Exercise_20 (21) = desimal to Octal

---
+++ src/Latihan/w3resource/BasicPart1/Exercise20.java
@@ -0,0 +1,13 @@
+package Latihan.w3resource.BasicPart1;
+
+public class Exercise20 {
+    // desimal to octal
+
+    public static void main(String[] args) {
+        int a = 15;
+
+        String octal = Integer.toOctalString(a);
+
+        System.out.println("octal dari = " + a + " adalah : " + octal);
+    }
+}
