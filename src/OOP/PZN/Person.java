51d9e21f4c6b5f69b8f9d62519756d4efc0efadf
yummicodes <yummicodes@gmail.com>
2021-05-15 21:27:09 +0700

    Constructor

--- src/OOP/PZN/Person.java
+++ src/OOP/PZN/Person.java
@@ -5,6 +5,12 @@ public class Person {
     String address;
     final String country = "Indonesia";
 
+    //Membuat Constructor
+    Person (String paramName, String paramAddress){
+        name = paramName;
+        address = paramAddress;
+    }
+
     // void = tidak mengembalikan data
     void sayHello(String nama){
         System.out.println("Hello " + nama + ", My name is " + name);
--- src/OOP/PZN/PersonApp.java
+++ src/OOP/PZN/PersonApp.java
@@ -5,10 +5,11 @@ public class PersonApp {
     public static void main(String[] args) {
         //Pembuatan Objek
         //1
-        Person person1 = new Person();
-        person1.name = "Abu Hammad"; // manipulasi
-        person1.address = "Nagrak";  // manipulasi / diubah
-        //person1.country = "Saudi Arabia"; // Akan ERROR karena field nya sudah FINAL. kita hanya bisa panggil
+        Person person1 = new Person("Abu Hammad", "Nagrak");
+        // data dibawah ini tidak diperlukan karena sudah menggunakan constructor
+//        person1.name = "Abu Hammad"; // manipulasi
+//        person1.address = "Nagrak";  // manipulasi / diubah
+//        //person1.country = "Saudi Arabia"; // Akan ERROR karena field nya sudah FINAL. kita hanya bisa panggil
 
             //Pemanggilan
         System.out.println(person1.name);
@@ -17,12 +18,11 @@ public class PersonApp {
 
         //2
         Person person2;
-        person2 = new Person();
-        person2.name = "asep";
+        person2 = new Person("Asep", "Bogor");
         person2.sayHello("Abu Hammad");
 
         //3
-        var person3 = new Person();
+        var person3 = new Person("Yumi", "Cikeas");
 
     }
 
