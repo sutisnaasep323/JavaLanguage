de59492deee6f248c8f7a57bb7eba3b68435f2a4
yummicodes <yummicodes@gmail.com>
2021-05-15 22:05:42 +0700

    KT_keyword Continue

--- src/KT/BreakContinuedanReturn_32.java
+++ src/KT/BreakContinuedanReturn_32.java
@@ -7,11 +7,13 @@ public class BreakContinuedanReturn_32 {
 
         while (true) {
 
+            a++; // ini dipindahkan ke sini karena ada keyword continue agar program tetap dieksekusi sesuai yang diinginkan
             if (a == 10) {
-                break;
+                break; // keyword untuk memaksa keluar dari loop
+            } else if (a == 5){
+                continue; // keyword untuk memaksa memulai aksi dari awal. ini tuh akan skip yang bawah dan mulai dari atas
             }
 
-            a++;
             System.out.println("Loop ke - " + a);
         }
 
