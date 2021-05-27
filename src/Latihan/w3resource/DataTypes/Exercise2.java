bb170cd50f033356aa1ee66e8dd99f03d4c46402
yummicodes <yummicodes@gmail.com>
2021-05-27 10:47:55 +0700

    Latihan_w3Resource_dataTypes_exercise2

---
+++ src/Latihan/w3resource/DataTypes/Exercise2.java
@@ -0,0 +1,11 @@
+package Latihan.w3resource.DataTypes;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        double a = 1000.0;
+        double inch = 0.0254;
+
+        double hasil = a * inch;
+        System.out.println(a + " Inch adalah " + hasil + " meter");
+    }
+}
