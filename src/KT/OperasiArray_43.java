0675a261687557011f407a143bde53118f580e29
yummicodes <yummicodes@gmail.com>
2021-07-05 08:56:28 +0700

    KT_OperasiArray_43_2

--- src/KT/OperasiArray_43.java
+++ src/KT/OperasiArray_43.java
@@ -42,9 +42,62 @@ public class OperasiArray_43 {
         printArray(arrayAngka4);
         System.out.println(arrayAngka4); //cetak memori
 
+            //fill array, adalah menjadikan isi array itu sama semua
+        System.out.println("\n=================\n\tFill Array\n=================");
+        int[] arrayAngka5 = new int[10];
+        printArray(arrayAngka5);
+        Arrays.fill(arrayAngka5,7);
+        printArray(arrayAngka5);
 
+        //Komparasi (membandingkan)
+        System.out.println("\n=================\nKomparasi Array\n=================");
+        int[] arrayAngka6 = {1,2,3,4,5};
+        int[] arrayAngka7 = {1,2,5,4,5};
 
+        System.out.println("\n>> Membandingkan dengan dua buah Array");
+        System.out.println(Arrays.equals(arrayAngka6,arrayAngka7));
 
+        if (Arrays.equals(arrayAngka6,arrayAngka7)){
+            System.out.println("Array sama");
+        } else {
+            System.out.println("Array beda");
+        }
+
+        System.out.println("\n>> Mengecek Array mana yang lebih besar");
+        /*
+        0 -> sama
+        > 0 -> array pertama lebih besar dari array kedua
+        < 0 -> array kedua lebih besar dari array pertama
+         */
+        System.out.println(Arrays.compare(arrayAngka6,arrayAngka7));
+
+        System.out.println("\n>> Mengecek index mana yang berbeda");
+        /*
+        -1 -> tidak ada yang berbeda
+        (disebutkan) -> index tersebut yang berbeda
+        Note : tetapi yang disebutkan adalah index pertama kali berbeda
+         */
+        System.out.println(Arrays.mismatch(arrayAngka6,arrayAngka7));
+
+        //Sort Array
+        System.out.println("\n=================\n\tSort Array\n=================");
+        int[] arrayAngka8 = {7,3,9,4,7,5,1,8};
+        printArray(arrayAngka8);
+        Arrays.sort(arrayAngka8);
+        printArray(arrayAngka8);
+
+        //Search Array, search menggunakan binarySearch tetapi harus diurutkan dulu datanya
+        System.out.println("\n=================\nSearch Array\n=================");
+        int searchAngka = 7;
+        int posisi = Arrays.binarySearch(arrayAngka8, searchAngka);
+        System.out.println("Angka " + searchAngka + " ada di index ke " + posisi);
+
+        /*
+        Latihan :
+        1. sort terbalik
+        2. operasi tambah antara dua buah array
+        3. Menggabungkan dua buah array
+         */
     }
 
     public static void printArray (int[] dataArray){
