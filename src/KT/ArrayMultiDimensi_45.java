a5be0b6d641b0de355e749d142e8021b82ebbf31
yummicodes <yummicodes@gmail.com>
2021-07-15 09:43:53 +0700

    KT_ArrayMultiDimensi_2_45

--- src/KT/ArrayMultiDimensi_45.java
+++ src/KT/ArrayMultiDimensi_45.java
@@ -15,7 +15,7 @@ public class ArrayMultiDimensi_45 {
         printArray_2D(array_2D);
 
         // Cara membuat Array 2D dengan deklarasi
-        // [baris][kolom]
+        // [baris][angka]
         int[][] array2_2D = new int[2][2];
 
         array2_2D[0][0] = 20;
@@ -24,15 +24,57 @@ public class ArrayMultiDimensi_45 {
         array2_2D[1][1] = 35;
         printArray_2D(array2_2D);
 
+        // Looping secara manual
+//        for (int i = 0; i < array2_2D.length; i++){
+//            System.out.println("=== Looping Manual ===");
+//            for (int j=0; j < array2_2D[i].length; j++){
+//                System.out.print(array2_2D[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+
+        // Looping dengan For Each
+//        System.out.println("=== Looping For Each ===");
+//        for (int[] baris : array2_2D){
+//            for(int angka : baris){
+//                System.out.print(angka + " ");
+//            }
+//            System.out.println();
+//        }
+
+        int[][] array3_2D = {
+                {1,4,2},
+                {6,2,8},
+                {3,1,5}
+        };
+        printArray_2D(array3_2D);
+
+        int[][] arrayRagged = {
+                {20,62,52,41,23},
+                {21,53,25},
+                {54}
+        };
+        printArray_2D(arrayRagged);
+        // mengapa arrayRagged di java itu bisa diprint? bukankah yang kita tahu array 2D itu jumlah komponen sama. insyaaAllah NEXT!
+
     }
 
 //    private static void printArray_2D (int[][] dataArray){
 //        System.out.println(Arrays.deepToString(dataArray));
 //    }
 
+//    private static void printArray_2D (int[][] dataArray){
+//        for (int i = 0; i < dataArray.length; i++){
+//            System.out.println(Arrays.toString(dataArray[i]));
+//        }
+//    }
+
     private static void printArray_2D (int[][] dataArray){
-        for (int i = 0; i < dataArray.length; i++){
-            System.out.println(Arrays.toString(dataArray[i]));
+        for (int[] baris : dataArray){
+            for(int angka : baris){
+                System.out.print(angka + " ");
+            }
+            System.out.println();
         }
     }
 }
