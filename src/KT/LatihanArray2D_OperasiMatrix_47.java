63da5541cc6375e7ed82db35c6d0eebdc11682d7
yummicodes <yummicodes@gmail.com>
2021-07-18 11:13:45 +0700

    KT_LatihanArray2D_OperasiMatrix_47_2

--- src/KT/LatihanArray2D_OperasiMatrix_47.java
+++ src/KT/LatihanArray2D_OperasiMatrix_47.java
@@ -20,8 +20,48 @@ public class LatihanArray2D_OperasiMatrix_47 {
 
         printArray(matrixA);
         printArray(matrixB);
-        int[][] hasil = tambahMatrix(matrixA,matrixB);
-        printArray(hasil);
+        int[][] hasilTambah = tambahMatrix(matrixA,matrixB);
+        printArray(hasilTambah);
+
+        int[][] matrixC = {
+                {1, 2},
+                {3, 4}
+        };
+
+        int[][] matrixD = {
+                {11, 12},
+                {13, 14}
+        };
+
+        int[][] hasilKali = kaliMatrix(matrixC, matrixD);
+        printArray(hasilKali);
+
+    }
+
+    public static int[][] kaliMatrix(int[][] matrix1, int[][] matrix2){
+        int baris_a = matrix1.length;
+        int kolom_a = matrix1[0].length;
+        int baris_b = matrix2.length;
+        int kolom_b = matrix2[0].length;
+
+        // jika a * b maka baris akan mengikuti baris a, kolom mengikuti kolom b
+        int[][] hasil = new int[baris_a][kolom_b];
+
+        if (kolom_a == baris_b) {
+            int buffer;
+            for (int i = 0; i < baris_a; i++) {
+                for (int j = 0; j < kolom_b; j++) {
+                    buffer = 0;
+                    for (int k = 0; k < kolom_a; k++) {
+                        buffer = buffer + (matrix1[i][k] * matrix2[k][j]);
+                    }
+                    hasil[i][j] = buffer;
+                }
+            }
+        } else {
+            System.out.println("Baris atau kolom tidak sama");
+        }
+        return hasil;
 
     }
 
@@ -39,6 +79,8 @@ public class LatihanArray2D_OperasiMatrix_47 {
                     hasil[i][j] = matrix1[i][j] + matrix2[i][j];
                 }
             }
+        } else {
+            System.out.println("Baris atau kolom tidak sama!");
         }
         return hasil;
     }
