684bebb3251f13226633e4094a06a6e94c58f701
yummicodes <yummicodes@gmail.com>
2021-07-18 11:50:53 +0700

    OOP_PZN_Method Everriding

--- src/OOP/PZN/Manager.java
+++ src/OOP/PZN/Manager.java
@@ -4,6 +4,6 @@ class Manager {
     String name;
 
     void sayHello(String name){
-        System.out.println("Hi, " + name + " My name is " + this.name);
+        System.out.println("Hi, " + name + " My name is Manager " + this.name);
     }
 }
--- src/OOP/PZN/VicePresident.java
+++ src/OOP/PZN/VicePresident.java
@@ -1,4 +1,9 @@
 package OOP.PZN;
 
 class VicePresident extends Manager{
+
+    void sayHello(String name){
+        System.out.println("Hi, " + name + " My name is VP " + this.name);
+    }
+
 }
