af50b703d36d0ed48937db3057d5db6610b445fd
Asep Sutisna <sutisnaasep323@gmail.com>
2022-01-19 16:53:27 +0700

    Udemy_PZN_S3_59_Field

--- src/Udemy/PZN/S3_JavaObjectOrientedProgramming/Person.java
+++ src/Udemy/PZN/S3_JavaObjectOrientedProgramming/Person.java
@@ -1,4 +1,7 @@
 package Udemy.PZN.S3_JavaObjectOrientedProgramming;
 
 public class Person {
+    String name;
+    String address;
+    final String country = "Indonesia";
 }
--- src/Udemy/PZN/S3_JavaObjectOrientedProgramming/PersonApp.java
+++ src/Udemy/PZN/S3_JavaObjectOrientedProgramming/PersonApp.java
@@ -3,12 +3,17 @@ package Udemy.PZN.S3_JavaObjectOrientedProgramming;
 public class PersonApp {
     public static void main(String[] args) {
         var person1 = new Person();
+        person1.name = "Yummi";
+        person1.address = "Nagrak";
+        // person1.country = "Japan" -> Error karena field FINAL
+
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+
         Person person2 = new Person();
         Person person3;
         person3 = new Person();
 
-        System.out.println(person1);
-        System.out.println(person2);
-        System.out.println(person3);
     }
 }
